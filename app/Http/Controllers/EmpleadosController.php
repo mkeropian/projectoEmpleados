452ab3e523f6f5d731d582406395a8c0dccfafda
Mariano <mkeropian@gmail.com>
2023-02-23 13:55:18 -0300

    Proyecto Empleados - Form Alta

---
+++ app/Http/Controllers/EmpleadosController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EmpleadosController extends Controller
+{
+
+    public function alta(){
+        $prov=DB::select(" select id_provincia,provincia from provincia ");
+        $doc=DB::select(" select id_tipo_documento, tipo_documento from tipo_documento ");
+        return view("alta")->with(["provincias" => $prov,"tipodocumento" => $doc]);
+    }
+
+    public function guardar( Request $request){
+        $sql=DB::insert(" insert into empleados ( apellido,nombre,direccion,localidad,id_tipo_documento,nrodocumento, codigopostal,id_provincia ) values ( ?,?,?,?,?,?,?,? ) ",[
+            $request -> apellido,
+            $request -> nombre,
+            $request -> direccion,
+            $request -> localidad,
+            $request -> id_tipo_documento,
+            $request -> nrodocumento, 
+            $request -> codigopostal,
+            $request -> id_provincia
+        ]);
+        if ($sql == true) {
+            return back() -> with ("correcto","Empleado registrado correctamente");
+        } else {
+            return back() -> with ("incorrecto","Error al registrar");
+        }
+        
+    }
+ 
+}
